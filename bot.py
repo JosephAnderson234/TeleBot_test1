f6ec72f5f6adea1d6f2700dc6239e97ade38f82e
JosephAnderson234 <coserojasj@gmail.com>
2022-02-09 20:22:26 -0500

    Update thye bot

--- bot.py
+++ bot.py
@@ -1,24 +1,21 @@
 import os
-from telegram.ext import Updater, MessageHandler, Filters
+from tkinter import Button
+from telegram.ext import Updater, CommandHandler
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ReplyMarkup
 
 
-def process_message(update, context):
-
-    text = update.message.text
-
-    if str(text).__contains__('#channel'):
-        context.bot.send_message(
-            chat_id='YOUR_CHANNEL_ID',
-            text=str(text).replace('#channel', '')
-        )
-
+def start(update, context):
+    
+    boton = InlineKeyboardButton(text="autor", url="https://portafolio2-jacr.web.app")
+    
+    update.message.reply_text(text="Hello, I'm a bot that can help you to find the best place to eat in your city.\n", reply_markup=InlineKeyboardMarkup([[boton]]))
 
 if __name__ == '__main__':
 
     updater = Updater(token=os.environ['TOKEN'], use_context=True)
 
     dp = updater.dispatcher
-    dp.add_handler(MessageHandler(filters=Filters.text, callback=process_message))
+    dp.add_handler(CommandHandler('start', start))
     updater.start_polling()
 
     print('Bot is polling')
